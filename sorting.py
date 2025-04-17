19bc46183a38a04c24577ef4f0e9f879ceedf87f
Miroslava Valouskova <257061@vutbr.cz>
2025-04-17 12:00:28 +0200

    function selection_sort

--- sorting.py
+++ sorting.py
@@ -1,3 +1,4 @@
+import csv
 import os
 
 
@@ -10,10 +11,48 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, "r") as f:
+        reader = csv.DictReader(f)
+        data = {"series_1":[], "series_2":[], "series_3":[]}
+        for row in reader:
+            #print(row)
+            for key, value in row.items():   #iterace přes slovník -> přes klíče
+                data[key].append(int(value))
+    return data
+
+
+# SELECTION SORT - nestabilní
+def selection_sort(numbers, direction):
+    n = len(numbers)
+
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if numbers[j] < numbers[min_idx] and direction == "asc":
+                min_idx = j
+            elif numbers[j] > numbers[min_idx] and direction == "desc":
+                min_idx = j
+
+        numbers[i], numbers[min_idx] = numbers[min_idx], numbers[i]
+
+    return numbers
+
+
+# BUBBLE SORT - stabilní
+
+
+
 
 
 def main():
-    pass
+    #print(read_data("numbers.csv"))
+    data = read_data("numbers.csv")
+    data_1 = data["series_1"]
+    data_2 = data["series_2"]
+    data_3 = data["series_3"]
+    print(selection_sort(data_1, "asc"))
+    print(selection_sort(data_2, "desc"))
+    print(selection_sort(data_3, "asc"))
 
 
 if __name__ == '__main__':
